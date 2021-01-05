db94df4ad3d515961f8ac5d7465ee66b60e4a814
emilypace26 <74545401+emilypace26@users.noreply.github.com>
2021-01-05 15:40:43 +0100

    created infinite game
    game resest when fish collides with seaweed
    Created a recycling system for the seaweed

---
+++ FlappyBird/Assets/Scripts/SeaweedCollisionHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SeaweedCollisionHandler : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("The fish has entered the seaweed");
+        //reload the scene when the fish hits the seaweed
+        SceneManager.LoadScene(0);
+    }
+}
