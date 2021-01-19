bb440bf9a61bad02120004dffab43ffb82c5124f
emilypace26 <74545401+emilypace26@users.noreply.github.com>
2021-01-19 15:18:15 +0100

    finalized flappy bird
    added score system
    added bubbles for extra points
    added the original scoring system

---
+++ FlappyBird/Assets/Scripts/Bubble.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bubble : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Reset();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.up * Time.deltaTime * _speed);
+        if(transform.position.y > 10)
+        {
+            Reset();
+        }
+    }
+
+    void Reset()
+    {
+        float randomHeight = Random.Range(-8f,-18f);
+        transform.position = new Vector3(transform.position.x,randomHeight,transform.position.z);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //we need to check if we collided with the player
+        if(other.tag == "Player")
+        {
+            //create reference to the scorekeeper script
+            ScoreKeeper scoreKeeper = GameObject.FindObjectOfType<ScoreKeeper>();
+            if(scoreKeeper != null)
+            {
+                //incrementScore
+                scoreKeeper.IncrementScore();
+            }
+            //Reset()
+            Reset();
+        }
+    }
+}
